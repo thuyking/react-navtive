2f1df60031540d855a51f3c25dcb3b8262b880f6
thuyking <162851590+thuyking@users.noreply.github.com>
2026-04-07 16:34:23 +0700

    tabBar and routing navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
 
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 justify-center items-center bg-background p-5">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
@@ -14,16 +17,14 @@ export default function App() {
       </Link>
       <Link href="/SignIn">Sign In</Link>
       <Link href="/SignUp">Sign Up</Link>
-      <Link href="/(tabs)/subscriptions/thuy">test</Link>
       <Link
         href={{
-          pathname: "/(tabs)/subscriptions/[id]",
+          pathname: "/subscriptions/[id]",
           params: { id: "thuy2" },
         }}
       >
-        {" "}
         test 2
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
-
+import "@/global.css";
 export default function Index() {
   return <Redirect href="/(tabs)" />;
 }
