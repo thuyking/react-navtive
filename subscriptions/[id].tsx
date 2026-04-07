5069e5bede7bf25daceda2c9bfb7963bd6266c90
thuyking <162851590+thuyking@users.noreply.github.com>
2026-04-07 17:04:55 +0700

    fix: tabBar

--- subscriptions/[id].tsx
+++ subscriptions/[id].tsx
@@ -1,14 +1,15 @@
 import { View, Text } from "react-native";
+import React from "react";
 import { Link, useLocalSearchParams } from "expo-router";
 
-const SubscriptionsDetail = () => {
+const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
   return (
     <View>
-      <Text>Subscriptions Detail: {id}</Text>
-      <Link href="/">Go back</Link>
+      <Text>Subscription Details: {id}</Text>
+      <Link href="/(auth)/HomePage">Go to the HomePage</Link>
     </View>
   );
 };
 
-export default SubscriptionsDetail;
+export default SubscriptionDetails;
